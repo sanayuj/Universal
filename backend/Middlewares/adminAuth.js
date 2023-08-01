44296214ded54bc5b438e5a5cdda3367c80a8b00
sanayuj <sanayuj2255@gmail.com>
2023-08-01 11:15:29 +0530

    model filename changed

--- backend/Middlewares/adminAuth.js
+++ backend/Middlewares/adminAuth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken')
 const AdminDB=require("../Model/adminModel")
 
 module.exports = async (req, res, next) => {
-  console.log("Admmin")
   try {
     const authHeader = req.headers.authorization
     const authtoken = authHeader.replace(/^Bearer\s+/i, '');
@@ -11,6 +10,7 @@ module.exports = async (req, res, next) => {
 
     //decodin the token
     const decoded = jwt.verify(authtoken, process.env.JWT_SECRETE_KEY)
+
     //checking whether user exist or not
     const admin = await AdminDB.findOne({ _id: decoded.id })
     if (!admin) return res.json({ loginfail: true, status: false, message: "Unauthorized token" })
